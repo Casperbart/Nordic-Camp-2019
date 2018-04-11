d4b67997ce13a17e47eee6bccaa90367a1c5d5a6
Søren Bjergmark <sahb1239@gmail.com>
2018-04-11 18:59:08 +0200

    Added documentation

--- src/Backend/Repository/IPageRepository.cs
+++ src/Backend/Repository/IPageRepository.cs
@@ -1,12 +1,35 @@
 ﻿using System.Threading.Tasks;
+using Backend.Exceptions;
 using Backend.Model;
 
 namespace Backend.Repository
 {
     public interface IPageRepository : IGenericRepository<Page>
     {
+        /// <summary>
+        /// Adds a page
+        /// </summary>
+        /// <param name="url">The url of the new page</param>
+        /// <param name="content">The content of the page</param>
+        /// <returns>The newly created page</returns>
+        /// <exception cref="PageAlreadyExistsException">Throws if the url is already present in the repository</exception>
         Task<Page> AddPage(string url, string content);
+
+        /// <summary>
+        /// Edits a page
+        /// </summary>
+        /// <param name="url">The url of the page to change</param>
+        /// <param name="content">The new content of the page</param>
+        /// <returns>The updated page</returns>
+        /// <exception cref="PageNotFoundException">Throws if the page was not found</exception>
         Task<Page> EditPage(string url, string content);
-        Task<Page> DeletePage(string url, string content);
+
+        /// <summary>
+        /// Deletes a page
+        /// </summary>
+        /// <param name="url">The url of the page to delete</param>
+        /// <returns></returns>
+        /// <exception cref="PageNotFoundException">Throws if the page was not found</exception>
+        Task DeletePage(string url);
     }
 }
