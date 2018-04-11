e64160e3d6ae7870f107ef863e4b11e70a652b93
Søren Bjergmark <sahb1239@gmail.com>
2018-04-11 19:50:03 +0200

    Initial work on UserModel

---
+++ src/Backend/Model/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Model
+{
+    /// <summary>
+    /// Common user model for volunteers and participants
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The unique userid used in the backend database
+        /// </summary>
+        [Key]
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// OpenID connect SubjectID - only releavant if user can login
+        /// If not connected to a user it is null
+        /// </summary>
+        public string SubjectId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
