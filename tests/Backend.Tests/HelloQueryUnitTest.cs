0738f3708837c94f2db715e4869eadbdb2491924
Søren Bjergmark <sahb1239@gmail.com>
2018-04-20 23:43:57 +0200

    Added GraphQL unit tests

---
+++ tests/Backend.Tests/HelloQueryUnitTest.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Backend.GraphQL.Helper.Schema.Base;
+using Backend.Tests.GraphQLMock;
+using Xunit;
+
+namespace Backend.Tests
+{
+    public class HelloQueryUnitTest : BaseGraphQLUnitTest
+    {
+        public HelloQueryUnitTest(ServiceProviderFixture serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [Fact]
+        public async Task TestQuery()
+        {
+            var queryOutput = await GraphQLClient.CreateQuery<HelloQuery>("URL_NOT_USED").Execute();
+            Assert.Equal("query", queryOutput.Hello);
+        }
+
+        public class HelloQuery
+        {
+            public string Hello { get; set; }
+        }
+    }
+}
