ea99719313289433b06b6a7b5c6eb50923971052
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 13:20:17 +0200

    Docstrings for exceptions

--- src/Backend/Exceptions/ItemAlreadyExistsException.cs
+++ src/Backend/Exceptions/ItemAlreadyExistsException.cs
@@ -2,6 +2,9 @@
 
 namespace Backend.Exceptions
 {
+    /// <summary>
+    /// Exception thrown when item already exists in a repository
+    /// </summary>
     public class ItemAlreadyExistsException : Exception
     {
         public ItemAlreadyExistsException() : base("Item already exists") { }
--- src/Backend/Exceptions/ItemNotFoundException.cs
+++ src/Backend/Exceptions/ItemNotFoundException.cs
@@ -2,6 +2,9 @@
 
 namespace Backend.Exceptions
 {
+    /// <summary>
+    /// Thrown if the item requested from the repository could not be found
+    /// </summary>
     public class ItemNotFoundException : Exception
     {
         public ItemNotFoundException() : base("Item not found") { }
