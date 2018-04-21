48fc4a401723b8bf129b29b3d0f2d82d1c5690d3
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 18:04:29 +0200

    Added docstrings on GraphQLResolverExtentions

--- src/Backend/GraphQL/Helper/GraphQlResolverExtentions.cs
+++ src/Backend/GraphQL/Helper/GraphQlResolverExtentions.cs
@@ -8,8 +8,19 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Backend.GraphQL.Helper
 {
+    /// <summary>
+    /// Contains extention methods for working with <see cref="IGraphQLExecutor"/> and <see cref="IGraphQLExecutor{T}"/>
+    /// </summary>
     public static class GraphQLResolverExtentions
     {
+        /// <summary>
+        /// Resolve the field using the <typeparamref name="TQueryExecutor"/>
+        /// </summary>
+        /// <typeparam name="TSource">The GraphQL source type</typeparam>
+        /// <typeparam name="TQueryExecutor">The query executor used to resolve</typeparam>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider"/> used to resolve the <typeparamref name="TQueryExecutor"/></param>
+        /// <param name="resolveFieldContext">The <see cref="ResolveFieldContext"/> used to resolve the output from the field</param>
+        /// <returns>Returns the resolved object</returns>
         public static async Task<object> ResolveUsingExecutor<TSource, TQueryExecutor>(this IServiceProvider serviceProvider, ResolveFieldContext resolveFieldContext)
             where TQueryExecutor : IGraphQLExecutor
         {
@@ -20,6 +31,14 @@ namespace Backend.GraphQL.Helper
             }
         }
 
+        /// <summary>
+        /// Resolve the field using the <typeparamref name="TQueryExecutor"/>
+        /// </summary>
+        /// <typeparam name="TSource">The GraphQL source type</typeparam>
+        /// <typeparam name="TQueryExecutor">The query executor used to resolve</typeparam>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider"/> used to resolve the <typeparamref name="TQueryExecutor"/></param>
+        /// <param name="resolveFieldContext">The <see cref="ResolveFieldContext{TSource}"/> used to resolve the output from the field</param>
+        /// <returns>Returns the resolved object</returns>
         public static async Task<object> ResolveUsingExecutor<TSource, TQueryExecutor>(this IServiceProvider serviceProvider, ResolveFieldContext<TSource> resolveFieldContext)
             where TQueryExecutor : IGraphQLExecutor<TSource>
         {
@@ -30,6 +49,14 @@ namespace Backend.GraphQL.Helper
             }
         }
 
+        /// <summary>
+        /// Resolve the field using the <typeparamref name="TQueryExecutor"/>
+        /// </summary>
+        /// <typeparam name="TSource">The GraphQL source type</typeparam>
+        /// <typeparam name="TQueryExecutor">The query executor used to resolve</typeparam>
+        /// <param name="fieldBuilder">The fieldBuilder which is used to build the field</param>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider"/> used to resolve the <typeparamref name="TQueryExecutor"/></param>
+        /// <returns>The same field builder which is used to build the field</returns>
         public static FieldBuilder<TSource, object> ResolveUsingExecutor<TSource, TQueryExecutor>(
             this FieldBuilder<TSource, object> fieldBuilder, IServiceProvider serviceProvider)
             where TQueryExecutor : IGraphQLExecutor<TSource>
