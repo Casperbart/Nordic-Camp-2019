1155055993ce4014f6e8f7731f7ca4ddb583094a
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 14:52:48 +0200

    Added ServiceProviderExtentions

---
+++ src/Backend/Helpers/ServiceProviderExtentions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Backend.Helpers
+{
+    public static class ServiceProviderExtentions
+    {
+        public static T Resolve<T>(this IServiceProvider serviceProvider)
+        {
+            return (T)Resolve(serviceProvider, typeof(T));
+        }
+
+        public static object Resolve(this IServiceProvider serviceProvider, Type type)
+        {
+            var serviceType = serviceProvider.GetService(type);
+            if (serviceType != null)
+                return serviceType;
+
+            // Check if constructor with 0 arguments was found
+            if (type.GetConstructors().Any(e => e.IsPublic && e.GetParameters().Length == 0))
+            {
+                return Activator.CreateInstance(type);
+            }
+
+            // Get all constructors
+            var constructors = type.GetConstructors().Where(e => e.IsPublic);
+            foreach (var constructor in constructors)
+            {
+                var arguments = new List<object>();
+                var foundAll = true;
+                foreach (var argument in constructor.GetParameters())
+                {
+                    var resolvedArgument = Resolve(serviceProvider, argument.ParameterType);
+                    if (resolvedArgument == null)
+                    {
+                        foundAll = false;
+                        break;
+                    }
+                    arguments.Add(resolvedArgument);
+                }
+
+                if (foundAll)
+                {
+                    return Activator.CreateInstance(type, arguments.ToArray());
+                }
+            }
+
+            throw new NotSupportedException("Cound not resolve constructor for type " + type);
+        }
+    }
+}
