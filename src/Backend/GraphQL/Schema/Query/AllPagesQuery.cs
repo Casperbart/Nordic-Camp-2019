654253fa39855958d37a47dcd98f0f31a63ece50
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 17:23:58 +0200

    Added AllPagesQuery

---
+++ src/Backend/GraphQL/Schema/Query/AllPagesQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Backend.GraphQL.Helper.Connections;
+using Backend.GraphQL.Helper.Schema;
+using Backend.GraphQL.Repository;
+using Backend.GraphQL.Types;
+using Backend.Model;
+
+namespace Backend.GraphQL.Schema.Query
+{
+    [RegistrerQuery]
+    public class AllPagesQuery : UnidirectionalConnectionQuery<PageGraphType, RepositoryUnidirectionalConnectionQueryExecutor<Page>>
+    {
+        public AllPagesQuery(IServiceProvider serviceProvider) : base(serviceProvider, "Pages")
+        {
+            Description = "Get all the pages";
+        }
+    }
+}
