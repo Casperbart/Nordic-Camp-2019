4e2d7ad2c4b2d7229da496f4b9802269f9d62eee
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 17:14:21 +0200

    Connection fields

---
+++ src/Backend/GraphQL/Helper/Connections/UnidirectionalConnectionQueryExecutor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Backend.GraphQL.Helper.Schema.Base;
+using GraphQL.Builders;
+using GraphQL.Types;
+
+namespace Backend.GraphQL.Helper.Connections
+{
+    /// <summary>
+    /// Contains a abstract unidirectional connection executor used to resolve connections
+    /// </summary>
+    public abstract class UnidirectionalConnectionQueryExecutor : IGraphQLExecutor
+    {
+        /// <summary>
+        /// The default page size, this can be overwritten
+        /// </summary>
+        protected int PageSize { get; set; } = 10;
+
+        /// <inheritdoc />
+        public Task<object> Resolve(ResolveFieldContext context)
+        {
+            var args = new ResolveConnectionContext<object>(context, true, PageSize);
+            CheckForErrors(args);
+
+            return Resolve(args);
+        }
+
+        /// <summary>
+        /// Resolves the connection by providing the <see cref="ResolveConnectionContext{T}"/>
+        /// </summary>
+        /// <param name="context">The Connection context which contains information of the cursors</param>
+        /// <returns>Returns the result from executing the field</returns>
+        protected abstract Task<object> Resolve(ResolveConnectionContext<object> context);
+
+        /// <summary>
+        /// Check the connection context from errors
+        /// </summary>
+        /// <param name="args">The connection context given from the user which should be validated</param>
+        protected void CheckForErrors(ResolveConnectionContext<object> args)
+        {
+            if (args.First.HasValue && args.Before != null)
+            {
+                throw new ArgumentException("Cannot specify both `first` and `before`.");
+            }
+            if (args.Last.HasValue && args.After != null)
+            {
+                throw new ArgumentException("Cannot specify both `last` and `after`.");
+            }
+            if (args.Before != null && args.After != null)
+            {
+                throw new ArgumentException("Cannot specify both `before` and `after`.");
+            }
+            if (args.First.HasValue && args.Last.HasValue)
+            {
+                throw new ArgumentException("Cannot specify both `first` and `last`.");
+            }
+            if (args.IsUnidirectional && (args.Last.HasValue || args.Before != null))
+            {
+                throw new ArgumentException("Cannot use `last` and `before` with unidirectional connections.");
+            }
+            if (args.IsPartial && args.NumberOfSkippedEntries.HasValue)
+            {
+                throw new ArgumentException("Cannot specify `numberOfSkippedEntries` with partial connection resolvers.");
+            }
+        }
+    }
+}
---
+++ src/Backend/GraphQL/Repository/RepositoryUnidirectionalConnectionQueryExecutor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.GraphQL.Helper.Connections;
+using Backend.Repository;
+using GraphQL.Builders;
+using GraphQL.Types.Relay.DataObjects;
+
+namespace Backend.GraphQL.Repository
+{
+    /// <summary>
+    /// Contains a UnidirectionalConnectionQueryExecutor which resolves from a <see cref="IGenericRepository{T}"/>
+    /// </summary>
+    /// <typeparam name="T">The type of the repository</typeparam>
+    public class RepositoryUnidirectionalConnectionQueryExecutor<T> : UnidirectionalConnectionQueryExecutor
+        where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+
+        /// <summary>
+        /// Constructs the executor with the specified repository as input parameter
+        /// </summary>
+        /// <param name="repository">The repository to resolve from</param>
+        public RepositoryUnidirectionalConnectionQueryExecutor(IGenericRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        /// <inheritdoc />
+        protected override async Task<object> Resolve(ResolveConnectionContext<object> context)
+        {
+            // Get repository page info
+            var repositoryPageInfo = await _repository.GetPageInfo(context.After, context.First ?? 10);
+
+            // Get repository edges
+            var repositoryEdges = await _repository.GetNodes(context.After, context.First ?? 10);
+
+            // Get edges
+            List<Edge<T>> edges = repositoryEdges.Select(e => new Edge<T>
+            {
+                Cursor = e.Cursor,
+                Node = e.Node
+            }).ToList();
+
+            // Get page info
+            PageInfo pageInfo = new PageInfo
+            {
+                StartCursor = repositoryPageInfo.StartCursor,
+                EndCursor = repositoryPageInfo.EndCursor,
+                HasNextPage = repositoryPageInfo.HasNextPage,
+                HasPreviousPage = repositoryPageInfo.HasPrevPage
+            };
+
+            // Return connection
+            return new Connection<T>
+            {
+                TotalCount = repositoryPageInfo.TotalCount,
+                PageInfo = pageInfo,
+                Edges = edges
+            };
+        }
+    }
+}
