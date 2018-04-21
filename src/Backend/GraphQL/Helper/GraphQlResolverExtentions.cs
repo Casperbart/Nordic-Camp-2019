fbb0f897ef3569cf0154ed02a69619a904a08825
Søren Bjergmark <sahb1239@gmail.com>
2018-04-21 14:56:53 +0200

    GraphQLResolverExtentions resolve using executor extention methods

---
+++ src/Backend/GraphQL/Helper/GraphQlResolverExtentions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Backend.GraphQL.Helper.Schema.Base;
+using Backend.Helpers;
+using GraphQL.Builders;
+using GraphQL.Types;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.GraphQL.Helper
+{
+    public static class GraphQLResolverExtentions
+    {
+        public static async Task<object> ResolveUsingExecutor<TSource, TQueryExecutor>(this IServiceProvider serviceProvider, ResolveFieldContext resolveFieldContext)
+            where TQueryExecutor : IGraphQLExecutor
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                TQueryExecutor graphQlExecutor = scope.ServiceProvider.Resolve<TQueryExecutor>();
+                return await graphQlExecutor.Resolve(resolveFieldContext).ConfigureAwait(false);
+            }
+        }
+
+        public static async Task<object> ResolveUsingExecutor<TSource, TQueryExecutor>(this IServiceProvider serviceProvider, ResolveFieldContext<TSource> resolveFieldContext)
+            where TQueryExecutor : IGraphQLExecutor<TSource>
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                TQueryExecutor graphQlExecutor = scope.ServiceProvider.Resolve<TQueryExecutor>();
+                return await graphQlExecutor.Resolve(resolveFieldContext).ConfigureAwait(false);
+            }
+        }
+
+        public static FieldBuilder<TSource, object> ResolveUsingExecutor<TSource, TQueryExecutor>(
+            this FieldBuilder<TSource, object> fieldBuilder, IServiceProvider serviceProvider)
+            where TQueryExecutor : IGraphQLExecutor<TSource>
+        {
+            return fieldBuilder.ResolveAsync(serviceProvider.ResolveUsingExecutor<TSource, TQueryExecutor>);
+        }
+    }
+}
--- src/Backend/GraphQL/Helper/Schema/Base/IGraphQLExecutor.cs
+++ src/Backend/GraphQL/Helper/Schema/Base/IGraphQLExecutor.cs
@@ -3,8 +3,31 @@ using GraphQL.Types;
 
 namespace Backend.GraphQL.Helper.Schema.Base
 {
+    /// <summary>
+    /// A GraphQL executor is used for resolving fields when a query is executed
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
     public interface IGraphQLExecutor
     {
+        /// <summary>
+        /// Resolves a field
+        /// </summary>
+        /// <param name="context">The <see cref="ResolveFieldContext"/> provided by GraphQL.NET</param>
+        /// <returns>Returns the type which should be mapped from</returns>
         Task<object> Resolve(ResolveFieldContext context);
+    }
+
+    /// <summary>
+    /// A GraphQL executor is used for resolving fields when a query is executed
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public interface IGraphQLExecutor<T>
+    {
+        /// <summary>
+        /// Resolves a field
+        /// </summary>
+        /// <param name="context">The <see cref="ResolveFieldContext{TSource}"/> provided by GraphQL.NET</param>
+        /// <returns>Returns the type which should be mapped from</returns>
+        Task<object> Resolve(ResolveFieldContext<T> context);
     }
 }
